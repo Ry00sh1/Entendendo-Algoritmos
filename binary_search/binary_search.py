ad0261d4ad97e2152bc5504eac79d645c99dec67
Pedro <phasena@gmail.com>
2024-08-06 20:52:09 -0300

    alteração no enunciado

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,4 +1,4 @@
-'''Implemente a função busca(lista, elemento), que busca um determinado elemento em uma lista e devolve o índice correspondente à posição do elemento encontrado. 
+'''Implemente a função busca_binaria(lista, elemento), que busca um determinado elemento em uma lista e devolve o índice correspondente à posição do elemento encontrado. 
 Utilize o algoritmo de busca binária. Nos casos em que o elemento buscado não existir na lista, a função deve devolver o booleano False.
 
 Além de devolver o índice correspondente à posição do elemento encontrado, sua função deve imprimir cada um dos índices testados pelo algoritmo.'''
